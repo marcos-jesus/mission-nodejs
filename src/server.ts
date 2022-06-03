ca543009acab40b70757c6c044d85a5e729e4a99
DevMarcosJesus <developermarcosjesus@gmail.com>
2022-06-03 14:30:55 -0300

    learn

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,21 @@ import express from "express"
 
 const app = express();
 
+
+/**
+ * Type params
+ * 
+ * Routes Params => http://localhost:3000/produtos/2312312
+ * Query Params => http://localhost:3000/produtos?name=teclado&description=tecladobom
+ * 
+ * Body Params => {
+ * "name":"teclado",
+ * description: "teclado bom"
+ * }
+ * 
+ */
+
+
 app.get("/teste", (request, response) => {
   return response.send("Olá NLW")
 })
